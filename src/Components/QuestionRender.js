e6911e0248d9da5b1fc717d4426f54fc19a4096a
TTatchell <tim.tatchell24@gmail.com>
2021-06-08 17:14:02 +1000

    Added a shuffle function for the multiple choice answers in QuestionRender

--- src/Components/QuestionRender.js
+++ src/Components/QuestionRender.js
@@ -1,25 +1,41 @@
 import { useEffect, useState } from 'react'
 import { decode } from 'he'
+import uuid from 'react-uuid'
 
 const QuestionRender = (props) => {
 
     const answerArray = [props.questionProp.correct_answer, ...props.questionProp.incorrect_answers]
 
+
+
+    //Thanks to javascriopt.info for the Fisher-Yates shuffle.
+    const shuffle = (array) => {
+        for (let i = array.length - 1; i > 0; i--) {
+            let j = Math.floor(Math.random() * (i + 1));
+            [array[i], array[j]] = [array[j], array[i]];
+        }
+        console.log(array)
+    }
+
+
+
+
     useEffect(() => {
-        console.log('QuestionRender.js updated')
-        console.log(answerArray)
-    })
+        console.log('Updating QuestionRender once')
+        shuffle(answerArray)
+        console.log(shuffle)
+    }, [])
 
-    
-    
 
-    
+
+
+
 
     return (
         <div>
             <h1>{decode(props.questionProp.question)}</h1>
 
-            {answerArray.map(answer => <h2>{answer}</h2>)}
+            {answerArray.map(answer => <h2 key={uuid()}>{decode(answer)}</h2>)}
 
         </div>
 
