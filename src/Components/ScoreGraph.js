c4cf99d2e2fd71983ff74f8461ccd36d1fe674a1
TTatchell <tim.tatchell24@gmail.com>
2021-06-14 00:24:46 +1000

    'ScoreGraph.js' now grabs data from server and displays it in a graph

--- src/Components/ScoreGraph.js
+++ src/Components/ScoreGraph.js
@@ -1,38 +1,62 @@
 import { Bar } from "react-chartjs-2";
-import GrabPrevScores from './GrabPrevScores'
+import { useState, useEffect } from 'react'
 
 const ScoreGraph = () => {
 
-    const data = GrabPrevScores()
 
+
+  const [scoreObject, setScoreObject] = useState({})
+  const [userNames, setUsernames] = useState([])
+  const [dataPack, setDataPack] = useState({})
+  const [scores, setScores] = useState([])
+
+  useEffect(() =>{
+    GrabPrevScores()
+  }, [])
+
+  useEffect(()=>{
+    let tmp = []
+    for (const key in scoreObject) {tmp = [...tmp, scoreObject[key]['Username']]}
+    setUsernames(tmp)
+
+    let tmp2 = []
+    for (const key in scoreObject) {tmp2 = [...tmp2, scoreObject[key]['Score']]}
+    setScores(tmp2)
+
+  }, [scoreObject])
+
+  useEffect(()=>{    
+    setDataPack({
+      labels: userNames,
+      datasets: [
+          {
+              label: 'Previous Score',
+              backgroundColor: 'rgba(75,192,192,1)',
+              borderColor: 'rgba(0,0,0,1)',
+              borderWidth: 2,
+              data: scores
+          }
+      ]
+  })
+  }, [userNames])
+
+  const GrabPrevScores = async () => {
+    console.log('running grab')
+    const url = 'http://localhost:3001/highscores'
+    const response =  await fetch(url)
+    const data = await response.json()
     console.log(data)
-
-    
-
-    const state = {
-        labels: ['January', 'February', 'March', 'April', 'May'],
-        datasets: [
-            {
-                label: 'Rainfall',
-                backgroundColor: 'rgba(75,192,192,1)',
-                borderColor: 'rgba(0,0,0,1)',
-                borderWidth: 2,
-                data: [65, 59, 80, 81, 56]
-            }
-        ]
-    }
-
-
-
-
+    setScoreObject(data)   
+}
     return (
         <div>
+          <button onClick={()=>GrabPrevScores()}>Show Scores</button>
         <Bar
-          data={state}
+          data={dataPack}
           options={{
             title:{
               display:true,
-              text:'Average Rainfall per month',
+              text:'Previously Set Scores',
               fontSize:20
             },
             legend:{
